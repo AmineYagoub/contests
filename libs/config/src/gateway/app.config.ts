6afe112ce464c37ca053fd3dd5a8f9cee40ccbe7
yagoub.2.amine@gmail.com <AmineYagoub@users.noreply.github.com>
2022-09-07 05:34:45 +0000

    ci(gateway): change gateway port to 3005

--- libs/config/src/gateway/app.config.ts
+++ libs/config/src/gateway/app.config.ts
@@ -6,7 +6,7 @@ export const GATEWAY_CONFIG_REGISTER_KEY = 'gatewayConfig';
 export const gatewayConfig = registerAs(GATEWAY_CONFIG_REGISTER_KEY, () => ({
   protocol: process.env.GATEWAY_PROTOCOL || 'http',
   host: process.env.GATEWAY_HOST || '127.0.0.1',
-  port: Number(process.env.GATEWAY_PORT) || 3000,
+  port: Number(process.env.GATEWAY_PORT) || 3005,
   get url() {
     return `${this.protocol}://${this.host}:${this.port}`;
   },
