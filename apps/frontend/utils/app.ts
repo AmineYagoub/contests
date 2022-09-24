fc9e784798f3c58cd00a8f56e7ae4c5ecba27881
Amine Yagoub <AmineYagoub@users.noreply.github.com>
2022-09-24 06:48:22 +0000

    feat: create global frontend logger

--- apps/frontend/utils/app.ts
+++ apps/frontend/utils/app.ts
@@ -1,6 +1,8 @@
+import { ValidateErrorEntity } from 'rc-field-form/es/interface';
 import { io, Socket } from 'socket.io-client';
-import { makeVar, ReactiveVar } from '@apollo/client';
+
 import { production } from '@/config/index';
+import { makeVar, ReactiveVar } from '@apollo/client';
 
 const url = production
   ? process.env.NEXT_PUBLIC_PROD_WS_ENDPOINT
@@ -15,3 +17,9 @@ export const socketVar: ReactiveVar<Socket> = makeVar(socket);
 
 export const getTitleMeta = (siteTitle: string, pageTitle?: string) =>
   pageTitle ? `${siteTitle} | ${pageTitle}` : `${siteTitle}`;
+
+export class Logger {
+  static log(error: Error | ValidateErrorEntity<unknown>) {
+    console.log(error);
+  }
+}
