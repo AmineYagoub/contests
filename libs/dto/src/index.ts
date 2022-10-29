77064966e7e5bb6db8fbf998ba9b65074621fe9f
Amine Yagoub <yagoub.2.amine@gmail.com>
2022-10-29 07:32:40 +0100

    build: remove prisma/contest-service client from auth service

--- libs/dto/src/index.ts
+++ libs/dto/src/index.ts
@@ -1,11 +1,11 @@
-export * from './lib/contests/create.dto';
+/* export * from './lib/contests/create.dto';
 export * from './lib/contests/pagination.dto';
 export * from './lib/contests/update.dto';
 export * from './lib/questions/create.dto';
 export * from './lib/questions/update.dto';
 export * from './lib/questions/pagination.dto';
 export * from './lib/answers/create.dto';
-export * from './lib/answers/update.dto';
+export * from './lib/answers/update.dto'; */
 export * from './lib/auth/signup.dto';
 export * from './lib/auth/signing.dto';
 export * from './lib/auth/signingWithGoogle.dto';
