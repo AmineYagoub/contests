e480e0c425837937c54cf81fe54cc6d8394c3635
Amine Yagoub <yagoub.2.amine@gmail.com>
2022-10-29 13:29:03 +0100

    build: include all types fron the libs dir

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/question.types';
-// export * from './lib/contest.types';
+export * from './lib/contest.types';
 export * from './lib/common.types';
 export * from './lib/pagination.types';
 export * from './lib/answer.types';
