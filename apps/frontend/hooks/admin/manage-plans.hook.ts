6e82dfcc63f9ebdcb1fa78a93e98db0161bd9413
Amine Yagoub <yagoub.2.amine@gmail.com>
2022-11-14 07:27:25 +0100

    refactor(hooks): rename student hooks to user hooks

---
+++ apps/frontend/hooks/admin/manage-plans.hook.ts
@@ -0,0 +1,82 @@
+import { Form } from 'antd';
+import {
+  SubscriptionPlan,
+  useCreateSubscriptionPlanMutation,
+  useFindAllSubscriptionPlansQuery,
+  useUpdateSubscriptionPlanMutation,
+} from '@/graphql/graphql';
+import { SubscriptionPlanActions } from '@/valtio/plans.state';
+import { Logger } from '@/utils/app';
+
+export interface CreatePlanProps {
+  visible?: boolean;
+  onClose?: () => void;
+  onSuccess?: () => void;
+  record?: SubscriptionPlan;
+}
+
+export const useCreateSubscriptionPlan = ({
+  record,
+  onClose,
+  onSuccess,
+}: CreatePlanProps) => {
+  const [form] = Form.useForm();
+  const [CreateSubscriptionPlanMutation, { loading, error }] =
+    useCreateSubscriptionPlanMutation();
+  const [
+    UpdateSubscriptionPlanMutation,
+    { loading: loadingUpdate, error: errorUpdate },
+  ] = useUpdateSubscriptionPlanMutation();
+
+  const onFinish = async () => {
+    try {
+      SubscriptionPlanActions.setMutationLoading(true);
+      const values = await form.validateFields();
+      const price = Number(values.price * 100);
+      const data = record
+        ? await UpdateSubscriptionPlanMutation({
+            variables: {
+              input: { ...values, price },
+              id: record.id,
+            },
+          })
+        : await CreateSubscriptionPlanMutation({
+            variables: {
+              input: { ...values, price },
+            },
+          });
+      if (data) {
+        form.resetFields();
+        onClose();
+        onSuccess();
+      }
+    } catch (error) {
+      Logger.log(error);
+    } finally {
+      SubscriptionPlanActions.setMutationLoading(false);
+    }
+  };
+
+  return {
+    onFinish,
+    form,
+    loading,
+    error,
+    loadingUpdate,
+    errorUpdate,
+  };
+};
+
+export const useFindPlans = () => {
+  const { data, loading, refetch } = useFindAllSubscriptionPlansQuery({
+    variables: {},
+    ssr: false,
+  });
+
+  return {
+    refetch,
+    data:
+      data?.findAllSubscriptionPlans?.map((d) => ({ key: d.id, ...d })) ?? [],
+    loading,
+  };
+};
