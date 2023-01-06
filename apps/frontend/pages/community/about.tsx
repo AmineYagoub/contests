6d6396c104ad8f62a8c637e1dda29561ba0f68fc
Amine Yagoub <yagoub.2.amine@gmail.com>
2023-01-06 08:09:55 +0100

    feat: create static pages

---
+++ apps/frontend/pages/community/about.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import { useReactiveVar } from '@apollo/client';
+import { appDataVar, getTitleMeta } from '@/utils/app';
+import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
+import HomeLayout from '@/layout/HomeLayout';
+import styled from '@emotion/styled';
+
+const StyledSection = styled('section')({
+  backgroundColor: '#f8f8f8 !important',
+  padding: '1em 5em',
+  p: {
+    whiteSpace: 'normal',
+    fontSize: '1.1rem',
+  },
+});
+
+export function AboutUsPage() {
+  const siteData = useReactiveVar(appDataVar);
+  return (
+    <StyledSection>
+      <Head>
+        <title>{getTitleMeta(siteData?.title, 'إتفاقية الإستخدام')}</title>
+      </Head>
+      <p
+        style={{ direction: 'rtl', whiteSpace: 'pre-wrap' }}
+        dangerouslySetInnerHTML={{ __html: siteData?.agreement }}
+      ></p>
+    </StyledSection>
+  );
+}
+
+AboutUsPage.getLayout = (page: EmotionJSX.Element) => (
+  <HomeLayout>{page}</HomeLayout>
+);
+export default AboutUsPage;
+// TODO get data by ssr
