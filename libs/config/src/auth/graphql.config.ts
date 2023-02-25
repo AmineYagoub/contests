3d4a83376f9d5d23995451d7442f926502722cac
Amine Yagoub <yagoub.2.amine@gmail.com>
2023-02-25 22:30:50 +0100

    fix(api): the schema is not a valid GraphQL schema

--- libs/config/src/auth/graphql.config.ts
+++ libs/config/src/auth/graphql.config.ts
@@ -1,4 +1,5 @@
 import { Inject } from '@nestjs/common';
+import { Federation2Config } from '@nestjs/graphql';
 import { ConfigType, registerAs } from '@nestjs/config';
 import { ApolloServerPluginInlineTraceDisabled } from 'apollo-server-core';
 import { isProd } from '../';
@@ -6,7 +7,11 @@ import { isProd } from '../';
 export const AUTH_GQL_REGISTER_KEY = 'authGQLConfig';
 
 export const authGQLConfig = registerAs(AUTH_GQL_REGISTER_KEY, () => ({
-  autoSchemaFile: true,
+  autoSchemaFile: {
+    federation: {
+      version: 2,
+    } as Federation2Config,
+  },
   cors: false,
   graphiql: !isProd,
   federationMetadata: true,
