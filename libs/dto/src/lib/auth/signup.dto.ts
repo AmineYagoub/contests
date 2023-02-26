fa431588cb4566cc23ba201ade495ac8917cfba7
Amine Yagoub <yagoub.2.amine@gmail.com>
2023-02-26 09:22:36 +0100

    fix(auth): the first parameter for @isEmail should be object

--- libs/dto/src/lib/auth/signup.dto.ts
+++ libs/dto/src/lib/auth/signup.dto.ts
@@ -12,7 +12,7 @@ import { Field, InputType } from '@nestjs/graphql';
 export class SignUpDto {
   @Field()
   @IsNotEmpty()
-  @IsEmail(null, { message: 'email_invalid' })
+  @IsEmail({}, { message: 'email_invalid' })
   email: string;
 
   @Field()
